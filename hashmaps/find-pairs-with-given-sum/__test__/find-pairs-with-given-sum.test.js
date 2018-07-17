baf7b7c9dc9f9389046ea5e716c2a88d297cd1e7
Sarah Geyer <sjgeyer@comcast.net>
2018-07-17 15:32:59 -0700

    add error checking to find pairs with given sums

--- hashmaps/find-pairs-with-given-sum/__test__/find-pairs-with-given-sum.test.js
+++ hashmaps/find-pairs-with-given-sum/__test__/find-pairs-with-given-sum.test.js
@@ -9,4 +9,13 @@ describe('find pairs with given sum', () => {
     const result = findPairs(array1, array2, 9);
     expect(result).toHaveLength(3);
   });
+  test('should throw error if no x passed', () => {
+    expect(() => findPairs([1, 2], [3, 4])).toThrow('invalid input. must define x');
+  });
+  test('should throw error if invalid data type passed', () => {
+    expect(() => findPairs({}, [2, 3], 9)).toThrow('invalid data type');
+  });
+  test('should throw error if arrays are empty', () => {
+    expect(() => findPairs([1, 2], [], 3)).toThrow('invalid input. arrays must not be empty');
+  });
 });
--- hashmaps/find-pairs-with-given-sum/find-pairs-with-given-sum.js
+++ hashmaps/find-pairs-with-given-sum/find-pairs-with-given-sum.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const findPairs = (a, b, x) => {
+  if (!Array.isArray(a) || !Array.isArray(b)) throw new Error('invalid data type');
+  if (a.length < 1 || b.length < 1) throw new Error('invalid input. arrays must not be empty');
+  if (!x) throw new Error('invalid input. must define x');
   const map = {};
   map[x] = [];
   for (let i = 0; i < a.length; i++) {
